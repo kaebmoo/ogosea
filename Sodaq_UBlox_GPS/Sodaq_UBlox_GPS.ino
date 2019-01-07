05c6af5289932e19eb4dd47aad6eecef46cf631b
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2019-01-07 08:40:45 +0700

    add universal tracker, add battery check

--- Sodaq_UBlox_GPS/Sodaq_UBlox_GPS.ino
+++ Sodaq_UBlox_GPS/Sodaq_UBlox_GPS.ino
@@ -1,5 +1,43 @@
+/*
+Copyright (c) 2017, SODAQ
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <Arduino.h>
 #include <Sodaq_UBlox_GPS.h>
+
+
+#define ADC_AREF 3.3f
+// #define BATVOLT_R1 2.0f // One v1
+// #define BATVOLT_R2 2.0f // One v1
+#define BATVOLT_R1 4.7f // One v2
+#define BATVOLT_R2 10.0f // One v2
+#define BATVOLT_PIN BAT_VOLT
 
 #define MySerial        SERIAL_PORT_MONITOR
 #define ARRAY_DIM(arr)  (sizeof(arr) / sizeof(arr[0]))
@@ -65,11 +103,12 @@ void setup()
 
     // This is for debugging to see more details, more messages
     // Use this in combination with setDiag()
-    //sodaq_gps.setMinNumOfLines(10);
+    // sodaq_gps.setMinNumOfLines(10);
 
     // Uncomment the next line if you want to see the incoming $GPxxx messages
-    //sodaq_gps.setDiag(MySerial);
+    // sodaq_gps.setDiag(MySerial);
 
+    SerialUSB.println(getBatteryVoltage());
     // First time finding a fix
     find_fix(0);
 }
@@ -87,6 +126,7 @@ void loop()
       // save the last time you blinked the LED
       previousMillis = currentMillis;
       do_flash_led(LED_RED);
+      SerialUSB.println(getBatteryVoltage());
       find_fix(0);
     }
 }
@@ -123,4 +163,11 @@ void do_flash_led(int pin)
         delay(100);
         digitalWrite(pin, HIGH);
     }
+}
+
+uint16_t getBatteryVoltage()
+{
+    uint16_t voltage = (uint16_t)((ADC_AREF / 1.023) * (BATVOLT_R1 + BATVOLT_R2) / BATVOLT_R2 * (float)analogRead(BATVOLT_PIN));
+
+    return voltage;
 }
